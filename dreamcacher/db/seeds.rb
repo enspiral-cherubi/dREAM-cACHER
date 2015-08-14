d0964bf62d889ea8161a09e59a4c0a58b6fe7282
SkwynethPaltrow <will.sklenars@gmail.com>
2015-08-14 12:45:23 +1200

    working on seeds

--- dreamcacher/db/seeds.rb
+++ dreamcacher/db/seeds.rb
@@ -1,3 +1,16 @@
+User.destroy_all
+Dream.destroy_all
+Tag.destroy_all
+
+tags = ["fuck","teeth","money","sex","food","music","love","hate","gold","bars","tsunami","earthquake","party"]
+
+13.times do |i|
+  Tag.create(word: tags[i])
+end
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
