145b859dd8167a2b5ec3686a1336a4c3bf6c4b4f
SkwynethPaltrow <will.sklenars@gmail.com>
2015-08-14 14:53:23 +1200

    built seed file

--- dreamcacher/db/schema.rb
+++ dreamcacher/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813231851) do
+ActiveRecord::Schema.define(version: 20150814015925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,14 @@ ActiveRecord::Schema.define(version: 20150813231851) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.integer  "dream_id"
     t.string   "word"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "themes", force: :cascade do |t|
+    t.integer  "dream_id"
+    t.integer  "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- dreamcacher/db/seeds.rb
+++ dreamcacher/db/seeds.rb
@@ -1,12 +1,48 @@
 User.destroy_all
 Dream.destroy_all
 Tag.destroy_all
+Theme.destroy_all
+
+users = []
+tags = []
+dreams = []
 
 tags = ["fuck","teeth","money","sex","food","music","love","hate","gold","bars","tsunami","earthquake","party"]
 
-13.times do |i|
-  Tag.create(word: tags[i])
+tags.length.times do |i|
+  tag = Tag.create(word: tags[i])
 end
+
+
+10.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    user_name: Faker::Internet.user_name,
+    password: Faker::Internet.password(8)
+  )
+  users.push user
+end
+
+2.times do
+  10.times do | i |
+    dream = Dream.create(
+      contents: Faker::Lorem.paragraph,
+      sentiment: 0.5,
+      user_id: users[i].id,
+    )
+    dreams.push dream
+  end
+end
+
+3.times do
+  dreams.length.times do | i |
+    Theme.create(
+      dream_id: dreams[i],
+      tag_id: tags.sample
+    )
+  end
+end
+
 
 
 
