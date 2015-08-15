ee89b46a76832d691ab9f769d1dd811516dd402d
SkwynethPaltrow <will.sklenars@gmail.com>
2015-08-16 00:55:56 +1200

    setting up javascript file structure

---
+++ dreamcacher/app/assets/javascripts/pages/router.js
@@ -0,0 +1,10 @@
+$(".pages").ready(function() {
+  // load the scene
+  // call a function to load the standard dreamscape
+
+  alert('.pages')
+})
+
+// make a route to load all dreams of a tag
+
+//make a route to load the standard dream function
