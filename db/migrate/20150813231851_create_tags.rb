cd800c1e05b3b4f58e5451db501c14f28ee904c2
will-sklenars <will.sklenars@gmail.com>
2016-02-21 19:03:26 +1300

    added t.appears_more_than_once :boolean, default: false to tags migration

--- db/migrate/20150813231851_create_tags.rb
+++ db/migrate/20150813231851_create_tags.rb
@@ -2,6 +2,7 @@ class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
       t.string :word, index: true
+      t.appears_more_than_once :boolean, default: false
       t.timestamps null: false
     end
   end
