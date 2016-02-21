851cbd83ab4a2ed88c52160d26169c83e44294bd
will-sklenars <will.sklenars@gmail.com>
2016-02-21 19:27:18 +1300

    updated tags_controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,6 +3,7 @@ class TagsController < ApplicationController
     dream = Dream.find(params['dream_id'])
     themes = dream.themes
     @tags = themes.map { | theme | theme.tag }
+    @tags.keep_if { | tag | tag.appears_more_than_once }
     render json: @tags
   end
 end
