239b27696796eb8b032424c42888eb989ea78949
will-sklenars <will.sklenars@gmail.com>
2016-02-21 19:28:25 +1300

    updated migration

--- db/migrate/20150813231851_create_tags.rb
+++ db/migrate/20150813231851_create_tags.rb
@@ -2,7 +2,7 @@ class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
       t.string :word, index: true
-      t.appears_more_than_once :boolean, default: false
+      t.boolean :appears_more_than_once, default: false
       t.timestamps null: false
     end
   end
