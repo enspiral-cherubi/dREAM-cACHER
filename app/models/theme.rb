f0adafbef58c97c7d11fe353b656293de3d4ade8
euglazer <eugenestapler@gmail.com>
2016-02-22 22:51:29 -0800

    increment counter_cache post Theme#create

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,12 @@
 class Theme < ActiveRecord::Base
   belongs_to :dream
   belongs_to :tag
+
+  after_create :increment_counter_cache
+
+  private
+    def increment_counter_cache
+      self.tag.dreams_count += 1
+      self.tag.save
+    end
 end
