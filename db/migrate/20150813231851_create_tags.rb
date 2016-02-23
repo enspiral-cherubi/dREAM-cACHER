cd2f6550b52fc929c585fe42050b7da8e4e839db
euglazer <eugenestapler@gmail.com>
2016-02-22 22:44:54 -0800

    remove appears_more_than_once from tags table

--- db/migrate/20150813231851_create_tags.rb
+++ db/migrate/20150813231851_create_tags.rb
@@ -2,7 +2,6 @@ class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
       t.string :word, index: true
-      t.boolean :appears_more_than_once, default: false
       t.timestamps null: false
     end
   end
